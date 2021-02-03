9cea6449e090b231e4fab1c866fec720c8780c9a
Gabriel Eguiguren <gabo.egui@gmail.com>
2021-02-03 15:34:07 -0500

    CORS configuritation, permite que otros dominios se puedan comunicar con nuestro gateway de servicios

--- ude-micro-service-zuul-server/src/main/java/ec/pymeapps/micro/app/zuul/security/oauth/ResourceAuthorizationServerConfig.java
+++ ude-micro-service-zuul-server/src/main/java/ec/pymeapps/micro/app/zuul/security/oauth/ResourceAuthorizationServerConfig.java
@@ -1,9 +1,13 @@
 package ec.pymeapps.micro.app.zuul.security.oauth;
 
+import java.util.Arrays;
+
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
@@ -11,6 +15,10 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.R
 import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 /**
  *	Con @EnableResourceServer configuramos que vamos a conectarnos a un server de autorizacion
@@ -52,9 +60,10 @@ public class ResourceAuthorizationServerConfig extends ResourceServerConfigurerA
 				"/api/user/usuarios/{id}").hasAnyRole("ADMIN", "USER") 	// usuarios autenticados
 		.antMatchers("/api/productos/**", "/api/items/**", 
 				"/api/user/usuarios/**").hasRole("ADMIN") 	// POST, PUT, DELETE solo para administradores
-		.anyRequest().authenticated();             //cualquier otra ruta no especificada aqui requiere autenticacion 						
+		.anyRequest().authenticated()    //cualquier otra ruta no especificada aqui requiere autenticacion
+		.and().cors().configurationSource(corsConfigurationSource()); // CORS para que otros servicios en otro domain pueda comunicarse              						
 	}
-
+	
 	@Bean
 	public JwtTokenStore tokenStore() {
 		return new JwtTokenStore(accessTokenConverter());
@@ -77,6 +86,43 @@ public class ResourceAuthorizationServerConfig extends ResourceServerConfigurerA
 		return tokenConverter;
 	}
 
+	/**
+	 * Configura que dominios externos podran acceder a nuestros endpoints,
+	 * por ejemplos clientes de Angular, React, etc
+	 * 
+	 * @return
+	 */
+	@Bean
+	public CorsConfigurationSource corsConfigurationSource() {
+		
+		CorsConfiguration corsConfig = new CorsConfiguration();
+		
+		corsConfig.setAllowedOrigins(Arrays.asList("*")); // por el momento se ponen todos, pero deberia ir un dominio: http://google/api
+		// metodos autorizados, donde "OPTIONS" es importante por que lo usa oauth2
+		corsConfig.setAllowedMethods(Arrays.asList("POST", "GET", "PUT", "OPTIONS"));
+		corsConfig.setAllowCredentials(true);
+		corsConfig.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
+		
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		
+		source.registerCorsConfiguration("/**", corsConfig); // Para que se aplique a todos nuestros ENDPOINTS: "/**"
+		return source;
+	}
+	
+	/**
+	 * Registra la configuracion de arriba en un Filter Bean
+	 * 
+	 * @return
+	 */
+	@Bean
+	public FilterRegistrationBean<CorsFilter> corsFilter(){
+		FilterRegistrationBean<CorsFilter> bean 
+		= new FilterRegistrationBean<CorsFilter>(new CorsFilter(corsConfigurationSource()));
+		
+		bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
+				
+		return bean;
+	}
 	
 	
 }
